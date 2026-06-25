98d2f59a906102e6061d26f4b749ae1ada10302f
Erwann Gache <egache@z4r3p2.42lyon.fr>
2026-06-25 16:52:22 +0200

    ex02 : start episode 2

--- ex02/includes/MutantStack.hpp
+++ ex02/includes/MutantStack.hpp
@@ -1,12 +1,34 @@
-#ifndef
+#ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-class MutantStack
+/*
+ empty()
+ size()
+ top()
+ push()
+ emplace()
+ pop()
+ swap()
+ ADD iterators() (const non const reverse)
+*/
+
+template <typename T> class MutantStack
 {
 private:
 public:
   MutantStack();
-  MutantStack(const Mutantstack &src);
+  MutantStack(const MutantStack &src);
   MutantStack &operator=(const MutantStack &rhs);
+
+  void empty(T t);
+  void size();
+  void top();
+  void push();
+  void emplace();
+  void pop();
+  void swap();
+
   ~MutantStack();
-}
+};
+
+#endif
