473ee8b9a1a5b75a37d7ba0fd2ec8ae19290f31a
Erwann Gache <egache@z4r5p7.42lyon.fr>
2026-06-26 18:48:32 +0200

    ex02 : added all iterator types. typedef matching subject requirement. added all method related

--- ex02/includes/MutantStack.hpp
+++ ex02/includes/MutantStack.hpp
@@ -2,25 +2,28 @@
 #define MUTANTSTACK_HPP
 
 #include <stack>
+#include <deque>
 
-/*
- ADD iterators() (const non const reverse)
-*/
-
-template <typename T, typename Container>
+template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container>
 {
 
 public:
   MutantStack();
-  MutantStack(const MutantStack &src);
-  MutantStack &operator=(const MutantStack &rhs);
-
-  typename std::stack<T, Container>::iterator iterator;
-  // iterator begin();
-  // iterator end();
-  // iterator
-
+  MutantStack(const MutantStack<T, Container> &src);
+  MutantStack &operator=(const MutantStack<T, Container> &rhs);
+  typedef typename std::stack<T, Container>::container_type::iterator iterator;
+  typedef typename std::stack<T, Container>::container_type::const_reverse_iterator const_reverse_iterator;
+  typedef typename std::stack<T, Container>::container_type::reverse_iterator reverse_iterator;
+  typedef typename std::stack<T, Container>::container_type::const_iterator const_iterator;
+  iterator begin();
+  iterator end();
+  reverse_iterator rbegin();
+  reverse_iterator rend();
+  const_iterator cbegin();
+  const_iterator cend();
+  const_reverse_iterator crbegin();
+  const_reverse_iterator crend();
   ~MutantStack();
 };
 
